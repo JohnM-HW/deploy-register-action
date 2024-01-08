a193b907df123f700463fd1ff45528c309ab73a1
John McCormack <john.mccormack@hostelworld.com>
2024-01-08 15:01:45 +0000

    first commit with structure

---
+++ .github/deploy-register-action/deploy-register.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import os
+from datetime import datetime
+
+payload = {
+    'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    'deployment_version': os.environ['GITHUB_SHA'][:8],
+    'app_name': os.environ['INPUT_APP_NAME'],
+    'rollback_version': '1.0.0'
+}
+
+#endpoint = os.environ['INPUT_APP_ENDPOINT']
+endpoint = 'https://europe-west1-hw-sre.cloudfunctions.net/deployments'
+headers = {'Content-Type': 'application/json'}
+
+response = requests.post(endpoint, data=json.dumps(payload), headers=headers)
+
+if response.status_code == 200:
+    print("Payload sent successfully!")
+else:
+    print(f"Failed to send payload. Status code: {response.status_code}")
