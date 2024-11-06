e87a4cf6078c3b86518e54f1f27d4a72a91bff56
John McCormack <john.mccormack@hostelworld.com>
2024-11-06 14:57:35 +0000

    x

---
+++ .github/deploy-register-action/send_payload.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from datetime import datetime
+import os
+
+def send_payload():
+    url = os.getenv('WEBHOOK_URL')
+    payload = {
+        'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'deployment_version': os.getenv('DEPLOYMENT_VERSION'),
+        'app_name': os.getenv('APP_NAME'),
+        'rollback_version': os.getenv('ROLLBACK_VERSION')
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    print(f'Status Code: {response.status_code}')
+    print(f'Response: {response.text}')
+
+if __name__ == "__main__":
+    send_payload()
